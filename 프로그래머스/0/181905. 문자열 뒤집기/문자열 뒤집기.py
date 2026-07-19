0a249a100fdf1146035a7ebcd6cae039c69c9734
kafka-snowflake <165086054+kafka-snowflake@users.noreply.github.com>
2026-07-19 19:48:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,2 +1,8 @@
 def solution(my_string, s, e):
-    return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+    """
+    P   r   o   g   r   a   2   1   S   r   e   m   m   3 
+    0   1   2   3   4   5   6   7   8   9  10  11  12  13
+    """
+    answer = my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+    # print(answer)
+    return answer
